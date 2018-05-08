9d1fd7b42e16ab325f368a9c84a42d47e5a0b4ba
関口昂平 <nightmare_jack0311@ezweb.ne.jp>
2018-05-08 12:58:04 +0900

    clock&resultlogo

--- HappyHomeDesigner/HappyHomeDesigner/clock.h
+++ HappyHomeDesigner/HappyHomeDesigner/clock.h
@@ -1,32 +1,44 @@
 //=============================================================================
 //
 // タイマー処理 [clock.h]
-// Author : 
+// Author : GP12B295 ⑫ 関口昂平
 //
 //=============================================================================
 #ifndef _CLOCK_H_
 #define _CLOCK_H_
 
 
+//*****************************************************************************
 // マクロ定義
+//*****************************************************************************
 #define	NUM_CLOCK				(2)			// ポリゴン数
 
-#define TEXTURE_GAME_CLOCK		("data/TEXTURE/bullet_R1.png")	// 画像
-#define TEXTURE_CLOCK_SIZE_X	(50/2)		// テクスチャサイズ
-#define TEXTURE_CLOCK_SIZE_Y	(50/2)		// 同上
+#define TEXTURE_GAME_CLOCK			("data/TEXTURE/clock.png")			// タイマー画像
+#define TEXTURE_GAME_CLOCKHAND		("data/TEXTURE/clockhand.png")		// 針画像
 
-#define TEXTURE_PATTERN_DIVIDE_X_CLOCK	(1)	// アニメパターンのテクスチャ内分割数（X)
-#define TEXTURE_PATTERN_DIVIDE_Y_CLOCK	(1)	// アニメパターンのテクスチャ内分割数（Y)
-#define ANIM_PATTERN_NUM_CLOCK			(TEXTURE_PATTERN_DIVIDE_X_CLOCK*TEXTURE_PATTERN_DIVIDE_Y_CLOCK)	// アニメーションパターン数
-#define TIME_ANIMATION_CLOCK			(4)	// アニメーションの切り替わるカウント
+#define CLOCK_POS_X				(60)
+#define CLOCK_POS_Y				(60)
 
-#define CLOCK_MAX						(1) // 敵の最大数
+#define	CLOCK_WIDTH		(100 / 2*SCREEN_SCALE)				// タイマーの幅
+#define	CLOCK_HEIGHT	(100 / 2*SCREEN_SCALE)				// タイマーの高さ
+
+#define TEXTURE_CLOCK_SIZE_X		(100/2)		// テクスチャサイズ
+#define TEXTURE_CLOCK_SIZE_Y		(100/2)		// 同上
+#define TEXTURE_CLOCKHAND_SIZE_X	(100/2)		// 同上
+#define TEXTURE_CLOCKHAND_SIZE_Y	(100/2)		// 同上
+
+#define TEXTURE_PATTERN_DIVIDE_X_CLOCKHAND	(1)	// アニメパターンのテクスチャ内分割数（X)
+#define TEXTURE_PATTERN_DIVIDE_Y_CLOCKHAND	(1)	// アニメパターンのテクスチャ内分割数（Y)
+#define ANIM_PATTERN_NUM_CLOCKHAND			(TEXTURE_PATTERN_DIVIDE_X_CLOCK*TEXTURE_PATTERN_DIVIDE_Y_CLOCK)	// アニメーションパターン数
+#define TIME_ANIMATION_CLOCKHAND			(4)	// アニメーションの切り替わるカウント
+
+#define CLOCK_MAX						(1) 
 
 //*****************************************************************************
 // 構造体宣言
 //*****************************************************************************
 
-typedef struct	// エネミー構造体
+typedef struct	// 針構造体
 {
 	bool			use;						// true:使用  false:未使用
 	D3DXVECTOR3		pos;						// ポリゴンの移動量
