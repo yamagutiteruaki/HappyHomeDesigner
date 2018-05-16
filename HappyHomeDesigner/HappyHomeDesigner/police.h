a3a8eb289d2195940dc1a628be575414f80efe8f
THS70913\chisakakouta <hal.kotachis@gmail.com>
2018-05-17 00:49:10 +0900

    ポリスのマクロ定義など修正

--- HappyHomeDesigner/HappyHomeDesigner/police.h
+++ HappyHomeDesigner/HappyHomeDesigner/police.h
@@ -11,7 +11,6 @@
 //*****************************************************************************
 // マクロ定義
 //*****************************************************************************
-
 // ポリス本体関係
 #define	VALUE_MOVE_POLICE		(1.00f)											// 移動速度係数
 #define	RATE_MOVE_POLICE			(0.20f)										// 移動慣性係数
@@ -29,17 +28,19 @@
 // ポリスアーム関係
 #define	POLICE_ARM_TYPE_MAX		(2)												// ポリスの腕の種類(右腕・左腕)
 #define	POLICE_ARM_MAX			(POLICE_MAX * 2)								// ポリスの腕の最大数(両腕の合計数)
-#define	POLICE_ARM_ANIM_FRAME	(1 * 60)										// アニメーション一往復にかかるフレーム数(秒数 * 60フレーム)
+#define	POLICE_ARM_ANIM_FRAME	(2 * 60 / 4)									// アニメーション一往復にかかるフレーム数(秒数 * 60フレーム)
+#define	POLICE_ARM_ANGLE		(D3DX_PI / 6 / 2)								// アニメーションの回転角度(180度 / 適当な角度)
 // ポリスのレッグ関係
 #define	POLICE_LEG_TYPE_MAX		(2)												// ポリスの足の種類(右足・左足)
 #define	POLICE_LEG_MAX			(POLICE_MAX * 2)								// ポリスの足の最大数(両足の合計数)
-#define	POLICE_LEG_ANIM_FRAME	(1 * 60)										// アニメーション一往復にかかるフレーム数(秒数 * 60フレーム)
+#define	POLICE_LEG_ANIM_FRAME	(2 * 60 / 4)									// アニメーション一往復にかかるフレーム数(秒数 * 60フレーム)
+#define	POLICE_LEG_ANGLE		(D3DX_PI / 6 / 2)								// アニメーションの回転角度(180度 / 適当な角度)
 // チェックポイント関係
 #define	CHECK_POINT_X_MAX		(3)												// 横方向のチェックポイントの最大数
 #define	CHECK_POINT_Y_MAX		(3)												// 縦方向のチェックポイントの最大数
 #define	CHECK_POINT_MAX			(CHECK_POINT_X_MAX * CHECK_POINT_Y_MAX)			// チェックポイントの合計
 //**************************************
-// 設定対象の構造体へのポインタ識別番号
+// アニメーション設定対象の識別番号
 //**************************************
 enum
 {
